5fb880cd765e90db7e27330d756e5ee568a93387
Chase <chsmatherly@gmail.com>
2023-09-01 15:44:14 -0700

    added firebase integration

---
+++ client/src/views/MyGroups.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+const MyGroups = (props) => {
+    return (
+        <>
+            
+        </>
+    )
+}
+export default MyGroups
